c745877e03249b3682d2a4d03a1b07258a9fda31
vaddelli88 <naveenvaddelli73@gmail.com>
2024-09-17 11:16:51 +0530

    Login pages Commit not login logic

---
+++ src/main/java/com/tenantmanagement/controller/login/ServiceLogin.java
@@ -0,0 +1,14 @@
+package com.tenantmanagement.controller.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ServiceLogin {
+	
+	@GetMapping("/serviceLogin")
+	public String serviceLogin() {
+		return "ServiceLogin";
+	}
+
+}
--- src/main/java/com/tenantmanagement/controller/login/TenantLoginController.java
+++ src/main/java/com/tenantmanagement/controller/login/TenantLoginController.java
@@ -1,4 +1,4 @@
-package com.tenantmanagement.controller;
+package com.tenantmanagement.controller.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TenantLoginController {
 
-    @GetMapping("/")
-    public String home() {
+    @GetMapping("/tenantLogin")
+    public String tenantLogin() {
         return "TenantLogin";  // This should correspond to your template name (TenantLogin.html).
     }
 }
